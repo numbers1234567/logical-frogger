2e2b1d19f4cb9efabd5f98959b568828d467579e
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2022-06-20 21:27:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <graphics/DynamicGraphics.hpp>
 
-int main() {
-    std::cout << "Bruh" << std::endl;
+int main(int argc, char const *argv[])
+{
+    DynamicGraphicsTarget window(sf::VideoMode(800, 600), "bruh", 0);
+    //window.setFramerateLimit(1);
+    while (window.isOpen()) {
+        window.pollAllEvents();
+        window.updateWindow();
+    }
+    
+    return 0;
 }
