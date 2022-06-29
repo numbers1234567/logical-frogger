2ce4444788b214453cd7a15398b8e1c743003566
numbers1234567 <33708611+numbers1234567@users.noreply.github.com>
2022-06-28 22:52:34 -0500

    Removed a little cringe

--- main.cpp
+++ main.cpp
@@ -1,58 +1,5 @@
-#include <graphics/DynamicGraphics.hpp>
-#include <graphics/DynamicGraphicsTarget.hpp>
-#include <graphics/DynamicText.hpp>
-#include <screens/ScreenTypes.hpp>
-#include <screens/GameOptionsScreen.hpp>
-#include <screens/GameScreen.hpp>
-#include <screens/StartScreen.hpp>
-#include <iostream>
-
-struct FullDataContainer
-{
-    DynamicGraphicsTarget& window;
-    StartScreen& startScreen;
-    GameOptionsScreen& gameOptionsScreen;
-    GameScreen& gameScreen;
-    FullDataContainer(DynamicGraphicsTarget& window,
-                      StartScreen& startScreen,
-                      GameOptionsScreen& gameOptionsScreen,
-                      GameScreen& gameScreen) 
-                      : window(window), startScreen(startScreen), 
-                      gameOptionsScreen(gameOptionsScreen), gameScreen(gameScreen) {}
-};
-
-int loop(FullDataContainer gameContainer) {
-    gameContainer.window.updateWindow();
-    return 0;
-}
-
-int mainloop() {
-    DynamicGraphicsTarget window(sf::VideoMode(1024, 1024), "bruh", 0);
-    StartScreen startScreen(&window);
-    GameOptionsScreen gameOptions(&window);
-    GameScreen gameScreen(&window);
-
-    FullDataContainer gameContainer(window, startScreen, gameOptions, gameScreen);
-
-    gameContainer.startScreen.setActive(nullptr);
-
-    while (!gameContainer.window.isFinished()) {
-        int ret = loop(gameContainer);
-        if (ret!=0) return ret;
-    }
-
-    return 0;
-}
-
-
-bool initResources() {
-    bool success=true;
-    success = success&&loadAllFonts();
-    return success;
-}
 
 int main(int argc, char const *argv[])
 {
-    initResources();
     return mainloop();
 }
